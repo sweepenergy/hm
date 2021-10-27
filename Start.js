2e2162d0ea6e075974db11459bb9b41ee8b2e33f
higiniojr <littleram100@gmail.com>
2021-10-26 19:26:44 -0700

    added free usage and free memory percentage

--- Start.js
+++ Start.js
@@ -4,6 +4,13 @@ var os = require('os-utils');
 os.cpuUsage(function(v){
     console.log( 'CPU Usage (%): ' + v+ '\n');
 });
+
+os.cpuFree(function(v){
+    console.log( 'CPU Free (%): ' + v+ '\n');
+});
+
+console.log(os.freememPercentage());
+
 exports.platform = function(){ 
     return process.platform;
 }
