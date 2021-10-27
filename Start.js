6d73bfe976fdfe65babc1589fd378ec832100962
higiniojr <littleram100@gmail.com>
2021-10-27 15:11:41 -0700

    added Memmory and Load

--- Start.js
+++ Start.js
@@ -1,15 +1,28 @@
-console.log("Congratulations! You got node.js to run");
+console.log("Congratulations! You got node.js to run" + '\n');
 var os = require('os-utils');
 
+//This gets the CPU Usage at a certain point
 os.cpuUsage(function(v){
-    console.log( 'CPU Usage (%): ' + v+ '\n');
+    console.log('CPU Usage (%): ' + v+ '\n');
 });
 
+//This gets the CPU Free at a certain point 
 os.cpuFree(function(v){
-    console.log( 'CPU Free (%): ' + v+ '\n');
+    console.log('CPU Free (%): ' + v+ '\n');
 });
 
-console.log(os.freememPercentage());
+//This gets the average load or bandwith
+console.log('Average Load: ' + os.loadavg(0.01) + '\n');
+
+//This gets the Total Memmory of the system
+console.log('Total Memmory: ' + os.totalmem() + '\n')
+
+//This gets the Free Memmory of the system
+console.log('Free Memmory: ' + os.freemem() + '\n')
+
+//This is the total free memmory percentage taken from CPU Usage and CPU Free
+console.log('Total Free Memmory (%): ' + os.freememPercentage() + '\n');
+
 
 exports.platform = function(){ 
     return process.platform;
