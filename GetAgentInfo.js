65611f7e1f12ab2158f0c4d9e9fb42862714571f
cgarcia311 <cgarcia311@ucmerced.edu>
2021-11-18 17:34:24 -0800

    adding send

--- GetAgentInfo.js
+++ GetAgentInfo.js
@@ -1,26 +1,33 @@
 function AgentStatus(){
-    
-}
-
-function GetMetrics() {
-    window.location.href='./MetricInformation.html'
-    xhttp= new XMLHttpRequest();
-    let text = "<table border= '1><tr><th>Name</th><th>Grade</th></tr>";
-    xhttp.open("GET","http://127.0.0.1:5000/all",true)
-    xhttp.send()
-    xhttp.onload = function(){
-        let data = JSON.parse(xhttp.reponse);
+    let text = "<table border='1'><tr><th>Agent Number</th><th>Location</th><th>Status</th></tr>";
+    const xhttp = new XMLHttpRequest();
+    const method = "GET";
+    const url= "http://127.0.0.1:5000/all";
+    const async = true;
+    xhttp.open(method, url, async);
+    xhttp.send();
+    xhttp.onload = () => {
+        let data = JSON.parse(xhttp.response);
         let keys = Object.keys(data);
-        for(let i =0;i<keys.length;i++){
-            text += "<tr><td>" +keys[i] +"</td><td>" + data[keys[i]] + "</td></tr>";
+        for (let i = 0; i < 3; i++){
+            if(keys[i] == keys["i"]){
+                "<th> off</th>"
+            }
+            else{
+            text += "<th>" + data[keys[i]]+"</th>";
         }
+        }   
         text += "</table>"
         document.getElementById("Metrics").innerHTML = text;
-    
     }
+    
+}
+function Test(){
+    document.getElementById("Metrics").innerHTML = "hello";
 }
 
-function ShowGrades(){
+
+function GetMetrics(){
     window.location.href='./MetricInformation.html'
     const xhttp = new XMLHttpRequest();
     const method = "GET";  // Could be GET, POST, PUT, DELETE, etc.
--- SendData.js
+++ SendData.js
@@ -3,9 +3,16 @@ var data = {
     "totalmemory": "3",
     "freememory": "3",
     }
+function GetMetrics(){
+    
+}
 function Send(){
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("POST", "SWEEPAPI");
+    xmlhttp.open("POST", "http://127.0.0.1:5000/");
     xmlhttp.setRequestHeader("Content-Type", "application/json");
     xmlhttp.send(JSON.stringify(data))
+}
+
+function Update(){
+    <meta http-equiv="refresh" content="60" ></meta>
 }
